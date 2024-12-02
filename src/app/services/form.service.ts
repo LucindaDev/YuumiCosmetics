cf7959336d6022ec463d726ac2a32b32767a4cf8
LucindaDev <lucindadsanchez@gmail.com>
2024-12-02 15:20:14 -0700

    add FormService with form submission logic and corresponding tests

---
+++ src/app/services/form.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormService {
+
+  constructor(private http: HttpClient ) {}
+
+  sendForm(data: any) {
+    return this.http.post('http://localhost:8080/form', data);
+  }
+
+}
